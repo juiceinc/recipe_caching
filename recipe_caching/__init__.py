b6c12c319421f0952a329b08355fbd9ff74589c1
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-11-20 17:24:29 -0600

    make recipe_caching log stuff with structlog, including ALL queries

--- recipe_caching/__init__.py
+++ recipe_caching/__init__.py
@@ -4,21 +4,8 @@ Recipe
 ~~~~~~~~~~~~~~~~~~~~~
 """
 import functools
-import logging
 
 from recipe_caching.caching_query import CachingQuery
-
-try:  # Python 2.7+
-    from logging import NullHandler
-except ImportError:
-
-    class NullHandler(logging.Handler):
-
-        def emit(self, record):
-            pass
-
-
-logging.getLogger(__name__).addHandler(NullHandler())
 
 
 def query_callable(regions, query_cls=CachingQuery):
